6bfd843c1f7a4a99863d03d3c4f87d1617efe366
Grant Schroeder <gschroeder@pznma.com>
2019-09-26 20:57:15 +0000

    Add scope for avaliable games

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,6 @@
 class Game < ApplicationRecord
   has_many :pieces
   has_many :users
-<<<<<<< HEAD
   scope :avaliable ~>{ where(avaliable: true ) }
   scope :unavaliable ~>{ where(avaliable: [nil, false] ) }
-=======
->>>>>>> dbb74f7019366c19d09fea47f120d2141792b2aa
 end
