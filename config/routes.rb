af0262cbe933ff39f12f272b6cd8b9e3893c0e3a
curokai <anadan20182019@gmail.com>
2019-10-01 16:03:36 +0000

    Root has been changed from ctest#index to static_pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,33 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'ctest#index'
+  root 'static_pages#index'
 
   resources :gameboard, :show 
   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+  # put your routes/resources before this line 
   get 'input_PointA_PointB' => 'check_obstruction_cases#input_PointA_PointB' 
   post 'is_obstruction' => 'check_obstruction_cases#is_obstruction'
 
-  resources :check_obstruction_cases_controller
+  #resources :check_obstruction_cases_controller
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
