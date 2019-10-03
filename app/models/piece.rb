08fd51391bfcef9d22c1b3ba05134a38069b3042
curokai <anadan20182019@gmail.com>
2019-10-03 04:54:23 +0000

    move_to()method has been created inside Piece model and it checks for If there is a piece occupying the location, and it is the opposite color, remove the piece from the chess board.

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,3 +1,29 @@
 class Piece < ApplicationRecord
-  belongs_to :users
+ #belongs_to :users
+     def move_to!(new_x, new_y)
+
+         if d=Piece.find_by(:x_position => new_x, :y_position => new_y)
+         userID=d.user_id
+         gameID=d.game_id
+         
+            if gameID==self.game_id && self.user_id!=userID # if GAME IS SAME AND Black vs White pieces
+             
+             self.x_position=new_x
+             self.y_position=new_y
+             self.save
+             d.destroy
+
+            else   # if GAME IS NOT SAME or SAME User's pieces         
+              "FAIL"
+
+            end 
+
+         else  # NO PIECE LOCATED THERE 
+           self.x_position=new_x
+           self.y_position=new_y
+           self.save
+         end
+      
+      end 
+
 end
