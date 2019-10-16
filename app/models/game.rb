b87c9119e4a30dc306d672f0b384f1ff69d99ce0
radamejr <radamejr@gmail.com>
2019-10-16 15:00:39 +0000

    Added rook valid_move

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,29 +11,29 @@ class Game < ApplicationRecord
     #white pieces
       #pawns
       (0..7).each do |x|
-        Pawn.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id)
+        Pawn.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id, color: "white")
       end
 
       #rooks
       [0, 7].each do |x|
-        Rook.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id)
+        Rook.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id, color: "white")
       end
    
       #Knights
       [1, 6].each do |x|
-        Knight.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id)
+        Knight.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id, color: "white")
       end
 
       #Bishops
       [2, 5].each do |x|
-      Bishop.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id)
+      Bishop.create(game_id: id, x_position: x, y_position: 7, user_id: white_player_id, color: "white")
       end
 
       #King
-      King.create(game_id: id, x_position: 4, y_position: 7, user_id: white_player_id)
+      King.create(game_id: id, x_position: 4, y_position: 7, user_id: white_player_id, color: "white")
       
       #Queen
-      Queen.create(game_id: id, x_position: 3, y_position: 7, user_id: white_player_id)
+      Queen.create(game_id: id, x_position: 3, y_position: 7, user_id: white_player_id, color: "white")
       
 
 
@@ -41,29 +41,29 @@ class Game < ApplicationRecord
      
       #pawns
       (0..7).each do |x|
-        Pawn.create(game_id: id, x_position: x, y_position: 2, user_id: black_player_id)
+        Pawn.create(game_id: id, x_position: x, y_position: 2, user_id: black_player_id, color: "black")
       end
 
       #rooks
       [0, 7].each do |x|
-        Rook.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id)
+        Rook.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id, color: "black")
       end
    
       #Knights
       [1, 6].each do |x|
-        Knight.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id)
+        Knight.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id, color: "black")
       end
 
       #Bishops
       [2, 5].each do |x|
-        Bishop.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id)
+        Bishop.create(game_id: id, x_position: x, y_position: 0, user_id: black_player_id, color: "black")
       end
 
       #King
-      King.create(game_id: id, x_position: 3, y_position: 0, user_id: black_player_id)
+      King.create(game_id: id, x_position: 3, y_position: 0, user_id: black_player_id, color: "black")
       
       #Queen
-      Queen.create(game_id: id, x_position: 4, y_position: 0, user_id: black_player_id)
+      Queen.create(game_id: id, x_position: 4, y_position: 0, user_id: black_player_id, color: "black")
 
   end
 
